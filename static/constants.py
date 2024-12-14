ddb043dcd034bc8ecf29f7c0c8ed1a1baa062c6c
joericks1998 <joericks1998@gmail.com>
2024-12-13 23:39:11 -0500

    changing repo structure

--- static/constants.py
+++ static/constants.py
@@ -3,4 +3,11 @@ nova_tfmr = 32
 dropout_rate = 0.1
 dff = 4*d_model
 num_heads = 4
+# vocab specs
+
 vocab_path = "static/vocabulary.txt"
+vocabulary = []
+with open(vocab_path, 'r') as file:
+    vocabulary = file.read().split('\n')
+
+stop_idx = vocabulary.index("<STOP>")
