aaa472703c54769be6e48a40cebf3d845f4ef465
joericks1998 <joericks1998@gmail.com>
2024-12-20 23:21:20 +1300

    creating base classes for encoder

--- encoding/encoding.py
+++ encoding/encoding.py
@@ -14,12 +14,32 @@ class Memory:
         return
 
 class Encoder(tf.Module):
-    def __init__(self, *cats, n_limit = None):
-        self.max_combos = len(clss)**n_limit
-        self.TransitionMatix = None
+    def __init__(self, tags, n_limit = None, predefinitions = {}):
+        self.max_combos = (len(tags)-1)**n_limit
+        self.TransitionMatrix = tf.zeros(shape = (1, len(tags)))
         self.TransitionStates = {}
-        self.cats = list(cats)
-        self.cat_idxs = [i for i in range(0, len(cats)-1)]
+        self.tags = {}
+        i = 0
+        for tag in tags[:len(tags)-1]:
+            self.tags[i] = tag
+            i+=1
+        self.predefinitions = predefinitions
+        self.tag_map = {}
+    def __call__(self, sequence):
+        #pretag sequences
+        tagged_data = []
+        for token in sequence:
+            tagged = False
+            for k in self.predefinitions.keys():
+                if token in self.predefinitions[k]:
+                    tagged_data.append((token, k))
+                    tagged = True
+            if not tagged:
+                tagged_data.append((token, None))
+        return dict(tagged_data)
 
-    def __call__(self, seq):
-        pass
+    def addTransition(self, tag_seq, target):
+        if target not in self.tags.values():
+            msg = "Invalid Target Tag"
+            raise ValueError(msg)
+        self.TransitionStates[self.TransitionMatrix.shape[0]] = tag_seq
