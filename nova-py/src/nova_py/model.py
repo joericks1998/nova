165cd9e7fb4b57b417d20cb35487b9d30424ce7c
joericks1998 <joericks1998@gmail.com>
2025-01-13 19:09:31 -0700

    added encoder and escape characters

--- nova-py/src/nova_py/model.py
+++ nova-py/src/nova_py/model.py
@@ -1,15 +1,22 @@
 import numpy as np
 import tensorflow as tf
 from .architecture import embedding, transformer, final
+from .semantics.parser import Encoder
 from pathlib import Path
 import yaml
 
 class Nova(tf.keras.Model):
-    def __init__(self, _hp_path = 'model/hyperparameters.yaml', _vocab_path = 'model/vocabulary.txt'):
+
+    @staticmethod
+    def modelInit(hp_path, vocab_path):
+        pass
+
+    def __init__(self, _hp_path = 'model/hyperparameters.yaml', _vocab_path = 'model/vocabulary.txt', _encoder_path = 'model/semantics'):
         super(Nova, self).__init__()
         parent = Path(__file__).resolve().parent
         self._hp_path = parent/_hp_path
         self._vocab_path = parent/_vocab_path
+        self._encoder_path = parent/_encoder_path
         with open(self._hp_path, 'r') as f:
             self.hp = yaml.safe_load(f)
         with open(self._vocab_path, 'r') as f:
@@ -21,13 +28,14 @@ class Nova(tf.keras.Model):
         self.tfmrs = {i+1: transformer.Layer(self.dims['d_model'], self.dims['num_heads'],
                                             self.dims['dff'], self.run_specs['dropout_rate']) for i in range(0,self.dims['num_transformers'])}
         self.final = final.Layer(len(self.vocabulary), self.dims['d_model'], self.run_specs['temperature'])
+        self.encoder = Encoder.load(path = self._encoder_path)
         # return
 
     def embedPass(self, in_batch):
         # embedding tokenized batch
         big_stack = []
         for seq in in_batch:
-            small_stack = tf.stack([self.embed(tkn) for tkn in seq])
+            small_stack = tf.stack([self.embedder(tkn) for tkn in seq])
             big_stack.append(small_stack)
         # stacking all batches into the embedding batch
         return tf.stack(big_stack)
@@ -38,20 +46,17 @@ class Nova(tf.keras.Model):
             fpass_batch = tfmr(fpass_batch)
         return fpass_batch
 
-    def fPass(self, in_batch, training = False):
+    def forwardPass(self, in_batch):
         #embed token batch
         embd_logits = self.embedPass(in_batch)
-
         # pass through transformer layers
         tfmr_logits = self.transformPass(embd_logits)
-
         # pass through last layer for probabilities and refiting
         probabilities = self.final(tfmr_logits)
-
         return probabilities
-
     #generate model outputs
     def generate(self, in_batch, training = False):
+
         pass
     #get config for serialization
     def get_config(self):
