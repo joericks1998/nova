5648995a78497b37fcf1942bcea001d122212684
joericks1998 <joericks1998@gmail.com>
2025-03-14 15:36:11 -0600

    making layernorm parameters trainable

--- nova-py/src/nova_py/architecture/transformer.py
+++ nova-py/src/nova_py/architecture/transformer.py
@@ -4,14 +4,15 @@ from . import attention, ffnn
 class Layer(tf.Module):
     def __init__(self, d_model, num_heads, dff, dropout_rate, name = None):
         super(Layer, self).__init__(name = name)
+        # initialize attention layer
         self.attention = attention.Layer(d_model=d_model, num_heads=num_heads)
+        # initialize deep layer
         self.ffnn = ffnn.Layer(d_model, dff)
-
-        # layer normalization
+        # create layer normalization
         self.layernorm = tf.keras.layers.LayerNormalization(epsilon = 1e6)
-        # dropout
+        self.layernorm.trainable = True
+        # create dropout
         self.dropout = tf.keras.layers.Dropout(dropout_rate)
-        self.built = True
     # @tf.function(reduce_retracing=True)
     def __call__(self, batch, training=False):
         # feed through attention mechanism
